cf6f1e3a85c039a48d38e28909ae3c2a991281b6
Armando Roberto Travieso Montes <armandortmontes@gmail.com>
2024-07-27 11:28:30 -0500

    solicitando pokes using axios and making styles

--- src/pages/home/header/Header.jsx
+++ src/pages/home/header/Header.jsx
@@ -1,11 +1,23 @@
+import Logo from '../../../assets/pokemon.png'
 import css from './header.module.scss'
 
 const Header = () => {
-  return (
-    <>
-      <h1>Header Component</h1>
-    </>
-  )
+    return (
+        <nav className={`navbar navbar-dark bg-dark ${css.contenedor}`}>
+            <div className="container justify-content-evenly">
+                <a className="navbar-brand" href="https://pokemongolive.com/" target="_blank">
+                    <img src={Logo} alt="logo" width={100}/>
+                </a>
+
+                <div className={css.formulario}>
+                    <form className="d-flex align-items-center" role="search">
+                        <input className={`form-control me-2 ${css.busqueda}`} type="search" placeholder="Type something" aria-label="Buscar" />
+                        <button className={`btn btn-outline-primary ${css.botonSearch}`} type="submit">Search Pokemon</button>
+                    </form>
+                </div>
+            </div> 
+        </nav>
+    )
 }
 
 export default Header
--- src/pages/home/layout/LayoutHome.jsx
+++ src/pages/home/layout/LayoutHome.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
-import css from './layout.module.scss'
+import axios from 'axios'
+import { URL_POKEMON } from '../../../api/apiRest'
+import { useEffect } from 'react'
 
 const LayoutHome = () => {
-  return (
-    <>
-      <h1>Layout Home</h1>
-    </>
-  )
+
+    {/** Calling PokeApi with Axios Library */ }
+    useEffect(() => {
+    
+        const api = async () => {
+
+            const pokeApi = await axios.get(`${URL_POKEMON}`);
+            console.log(pokeApi);
+        }
+
+         // caliing const 
+        api();
+    }, [])  
+
+    return (
+        <>
+            <h1>Layout Home</h1>
+        </>
+    )
 }
 
 export default LayoutHome
