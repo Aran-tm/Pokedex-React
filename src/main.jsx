e102031dbb08cf195a10f37a547cfa7cb51bd8ad
Armando Roberto Travieso Montes <armandortmontes@gmail.com>
2024-07-27 14:26:33 -0500

    borrando todo para proyecto futuro

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import LayoutHome from './pages/home/layout/LayoutHome'
-import Header from './pages/home/header/Header'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <Header />
-        <LayoutHome />
     </React.StrictMode>,
 )
